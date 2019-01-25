b168ae9402a15d43d860f64a0a26be3b0a2e3609
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-25 22:04:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  
+  for(var index = 0; index<musicians.length; index++){
+   var endIndex = index +1;
+   var musician = musicians.slice(index, endIndex);
+   var instrument = instruments.slice(index, endIndex);
+   var x = `${musician} plays ${instrument}`;
+ 
+  emptyArray.push(x);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(facts){
+  var index = 0;
+  var newEmptyArray = [];
+  
+  while(index<facts.length){
+  var endIndex = index + 1;
+  var pullOutElement = facts.slice(index, endIndex);
+  var addEx = `${pullOutElement}!!!`;
+  newEmptyArray.push(addEx);
+  
+  index = index + 1;
+  }
+  return newEmptyArray;
+}
+function iLoveTheBeatles(number){
+ var result = "";
+ // var i = 0;
+
+do {
+  number = number + 1;
+  result = result + number;
+} while (i < 15);
+
+console.log(result);
+// expected result: "12345"
+
+}
+
+
+
+
